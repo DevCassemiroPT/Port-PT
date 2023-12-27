55f5ae45598b362e738657269dd0da84445a0ec1
DevCassemiroPT <alexandre.140395@gmail.com>
2023-12-27 22:03:36 +0000

    Praticando Switch

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-/*
-var x = 0;
-while( x <= 10){
-    document.write("<br>O valor do x é: " + x);
-    x++;
-}
-*/
+//Switch
 
-var valor = 15
+function pedir(){
+    var valor = prompt("Digite o valor de 1 a 4");
 
-for(a = 4; a < valor ; a++){
-    document.write("<br> O valor do a é: " + a);
+    switch(Number(valor)){
+        case 1:
+            alert("Você escolheu 1 = Suco");
+        break;
+        case 2:
+            alert("Você escolheu 2 = Água");
+        break;
+        case 3:
+            alert("Você escolheu 3 = Leite");
+        break;
+        case 4:
+            alert("Você escolheu 4 = Refrigerante");
+        break;
+        default:
+            alert("Escolha uma opção entre 1 e 4.")
+    }
 }
