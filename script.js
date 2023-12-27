06533e54d0c6d5a26d42d1ac2c00b293fae712c5
DevCassemiroPT <alexandre.140395@gmail.com>
2023-12-27 20:36:06 +0000

    while and for

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+var x = 0;
+while( x <= 10){
+    document.write("<br>O valor do x é: " + x);
+    x++;
+}
+*/
+
+var valor = 15
+
+for(a = 4; a < valor ; a++){
+    document.write("<br> O valor do a é: " + a);
+}
